008d1d0c31836c6afd37ccac7348f993d140edbb
fredrickuser <user.fredricknicholas8@gmail.com>
2023-10-25 18:13:42 +0100

    initial commit

---
+++ src/data/testimonialsData.js
@@ -0,0 +1,25 @@
+import image1 from "../assets/t-image1.png";
+import image2 from "../assets/t-image2.jpg";
+import image3 from "../assets/t-image3.jpg";
+
+export const testimonialsData = [
+  {
+    image: image1,
+    review:
+      "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
+    name: 'MATHEW HENDRICKSON',
+    status : 'ENTREPRENEUR'
+  },
+  {
+    image: image2,
+    review: 'I have had amazing success with the Daily Routine Fitness VIP program, and couldn’t recommend it more! Rachel has helped me refine my approach to workouts, nutrition and overall motivation. ',
+    name: 'JOHN KEVIN',
+    status: 'COACH'
+  },
+  {
+    image : image3,
+    review:' I was one of those people that worked out daily, ate reasonably well, and looked fine (especially in clothes!).',
+    name: 'FRANKLIN',
+    status: "CUSTOMER"
+  }
+];
